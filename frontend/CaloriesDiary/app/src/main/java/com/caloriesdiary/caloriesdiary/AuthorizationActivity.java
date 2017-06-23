ca81c8a41346edd9c7066d3cb606a7d2791b3ee3
Ryuugamine <Alex.beliayev@gmail.com>
2017-06-23 15:24:03 +0300

    тестинг

---
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/AuthorizationActivity.java
@@ -0,0 +1,13 @@
+package com.caloriesdiary.caloriesdiary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class AuthorizationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorization_layout);
+    }
+}
