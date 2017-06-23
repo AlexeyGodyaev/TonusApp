c6c24356de0d79ab988c9368520bd667637dc888
BSOD_levch <levch98@mail.ru>
2017-06-23 12:40:58 +0300

    База данных заработала

--- backend/application/config/database.php
+++ backend/application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=postgres',
 	'hostname' => 'localhost',
 	'username' => 'postgres',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => 'postgres',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
