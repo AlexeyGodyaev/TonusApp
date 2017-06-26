45adae702b3250b2b639e8450da6c06a57752e9c
Ryuugamine <Alex.beliayev@gmail.com>
2017-06-26 10:01:25 +0300

    запилил окно регистрации

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/AuthorizationActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/AuthorizationActivity.java
@@ -1,13 +1,25 @@
 package com.caloriesdiary.caloriesdiary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class AuthorizationActivity extends Activity {
+
+    Button reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authorization_layout);
+        reg = (Button) findViewById(R.id.registrationButton);
+    }
+
+    public  void registrationClc(View view){
+        Intent intent = new Intent(getApplicationContext(),RegistrationActivity.class);
+
+        startActivity(intent);
     }
 }
