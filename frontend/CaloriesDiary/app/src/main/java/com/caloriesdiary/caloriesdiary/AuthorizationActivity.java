2bb2d396d6bb8e591c3decab3eba655bb4017725
HNKOP <emilgasanov@mail.ru>
2017-06-26 11:20:29 +0300

    Удалил какой-то мусор

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/AuthorizationActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/AuthorizationActivity.java
@@ -3,16 +3,12 @@ package com.caloriesdiary.caloriesdiary;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.view.Menu;
-=======
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
->>>>>>> Alex's-branch
 
 
 public class AuthorizationActivity extends Activity {
