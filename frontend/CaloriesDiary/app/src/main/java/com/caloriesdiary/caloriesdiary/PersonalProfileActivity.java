ff82eb29664622d584d04df9eaf7b88c5ad422e0
HNKOP <emilgasanov@mail.ru>
2017-06-27 15:11:38 +0300

    Личный профиль, Фоточки, глобальные настройки и прочее

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/PersonalProfileActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/PersonalProfileActivity.java
@@ -1,39 +1,52 @@
 package com.caloriesdiary.caloriesdiary;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Time;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.util.concurrent.ExecutionException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
-public class PersonalProfileActivity extends AppCompatActivity{
-    Button ok_btn;
-    Spinner spinner;
-    TextView name,age,height,weight;
-    RadioButton male,female;
-
+public class PersonalProfileActivity extends AppCompatActivity {
+    Button edit_photo_btn;
+    ImageView photo_view;
+    private final int reqcode = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_edit_layout);
+        setContentView(R.layout.personal_layout);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        ok_btn = (Button) findViewById(R.id.ok_btn);
-        spinner = (Spinner) findViewById(R.id.activity_spinner);
-        male = (RadioButton) findViewById(R.id.gender_male);
-        female = (RadioButton) findViewById(R.id.gender_female);
-        name = (TextView) findViewById(R.id.name_edit);
-        age = (TextView) findViewById(R.id.age_edit);
-        height = (TextView) findViewById(R.id.height_edit);
-        weight = (TextView) findViewById(R.id.weight_edit);
+        edit_photo_btn = (Button) findViewById(R.id.edit_photo_btn);
+        photo_view = (ImageView) findViewById(R.id.personal_photo);
 
+        File imgFile = new  File("/data/user/0/com.caloriesdiary.caloriesdiary/cache/avatar.jpg");
+
+        if(imgFile.exists()){
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+            ImageView myImage = (ImageView) findViewById(R.id.personal_photo);
+
+            myImage.setImageBitmap(myBitmap);
+
+        }
 
     }
     @Override
@@ -45,36 +58,57 @@ public class PersonalProfileActivity extends AppCompatActivity{
         }
         return super.onOptionsItemSelected(item);
     }
-    public void onClick(View view) throws InterruptedException, ExecutionException {
-        String selected_activity = String.valueOf(spinner.getSelectedItemPosition());
-        String selected_gender;
-        if(male.isChecked())
-        {
-            selected_gender = "1";
+
+    public void onClickEditPhoto(View view)
+    {
+        Toast.makeText(getApplicationContext(),this.getCacheDir().toString(),Toast.LENGTH_LONG).show();
+        Intent pickphoto = new Intent(Intent.ACTION_PICK);
+        pickphoto.setType("image/*");
+        startActivityForResult(pickphoto,reqcode);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case reqcode:
+                if(resultCode == RESULT_OK){
+                    try {
+
+                        //Получаем URI изображения, преобразуем его в Bitmap
+                        //объект и отображаем в элементе ImageView нашего интерфейса:
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        photo_view.setImageBitmap(selectedImage);
+                        SavePicture(photo_view,this.getCacheDir().toString());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }}
+
+    private String SavePicture(ImageView iv, String folderToSave)
+    {
+        OutputStream fOut = null;
+        Time time = new Time();
+        time.setToNow();
+
+        try {
+            File file = new File(folderToSave,"avatar.jpg"); // создать уникальное имя для файла основываясь на дате сохранения
+            fOut = new FileOutputStream(file);
+            iv.buildDrawingCache();
+            Bitmap bitmap = iv.getDrawingCache();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut); // сохранять картинку в jpeg-формате с 85% сжатия.
+            fOut.flush();
+            fOut.close();
+            MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), file.getName(),  file.getName()); // регистрация в фотоальбоме
+            Toast.makeText(getApplicationContext(),"Сохарненик",Toast.LENGTH_LONG).show();
         }
-        else if ( female.isChecked())
+        catch (Exception e) // здесь необходим блок отслеживания реальных ошибок и исключений, общий Exception приведен в качестве примера
         {
-            selected_gender = "2";
+            return e.getMessage();
         }
-        else
-        {
-            selected_gender = "3";
-        }
-        Post log = new Post();
-
-        String args [] = new String[7];
-
-        args[0] = "http://192.168.1.205/users/auth";  //аргументы для пост запроса
-        args[1] = name.getText().toString();
-        args[2] = age.getText().toString();
-        args[3] = selected_gender;
-        args[4] = height.getText().toString();
-        args[5] = weight.getText().toString();
-        args[6] = selected_activity;
-
-        log.execute(args); // вызываем запрос
-
-        //log.get().toString()
-        Toast.makeText(getApplicationContext(), log.get().toString() ,Toast.LENGTH_LONG ).show();
+        return "";
     }
 }
