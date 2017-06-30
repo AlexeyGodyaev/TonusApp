9f97fded059ca4c7de14fdd81918803f578bfe22
HNKOP <emilgasanov@mail.ru>
2017-06-30 16:49:36 +0300

    sdf

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/ActionsCatalogActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/ActionsCatalogActivity.java
@@ -4,6 +4,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -59,11 +62,11 @@ public class ActionsCatalogActivity extends FragmentActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 JSONObject jsn = new JSONObject();
                 TextView txtName = (TextView) view.findViewById(R.id.productName);
-                TextView txtCalories = (TextView) view.findViewById(R.id.productCalories);
+                final TextView txtCalories = (TextView) view.findViewById(R.id.productCalories);
                 try {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(ActionsCatalogActivity.this);
-                    builder.setTitle("Важное сообщение!")
-                            .setMessage("Покормите кота!")
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(ActionsCatalogActivity.this);
+                    builder.setTitle(txtName.getText().toString())
+                            .setMessage(txtCalories.getText().toString())
                             .setCancelable(false)
                             .setNegativeButton("ОК, иду на кухню",
                                     new DialogInterface.OnClickListener() {
@@ -72,12 +75,35 @@ public class ActionsCatalogActivity extends FragmentActivity {
                                         }
                                     });
 
+                    String str = txtCalories.getText().toString().substring(0,txtCalories.getText().toString().indexOf('.'));
+                    final int kcal = Integer.parseInt(str);
+
                     final EditText input = new EditText(ActionsCatalogActivity.this);
+                    input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    input.addTextChangedListener(new TextWatcher()
+                    {
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count)
+                        {
+                                int time = Integer.parseInt(input.getText().toString());
+                                int newkcal = kcal * time / 60;
+                                Toast.makeText(getApplicationContext(),String.valueOf(newkcal),Toast.LENGTH_LONG).show();
+                        }
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int aft )
+                        {
+
+                        }
+                        @Override
+                        public void afterTextChanged(Editable s)
+                        {
+
+                        }
+                    });
                     LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                             LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.MATCH_PARENT);
-                    input.setLayoutParams(lp);
-                    builder.setView(input);
+                    input.setLayoutParams(lp);                    builder.setView(input);
                     AlertDialog alert = builder.create();
                     alert.show();
 
