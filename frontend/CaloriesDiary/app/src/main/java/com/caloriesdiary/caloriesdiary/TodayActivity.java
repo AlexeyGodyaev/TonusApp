14320b913a128d9edd826245cacf80031d6ac0db
Ryuugamine <Alex.beliayev@gmail.com>
2017-07-04 16:41:54 +0300

    так блэт

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/TodayActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/TodayActivity.java
@@ -19,35 +19,37 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class TodayActivity extends FragmentActivity {
 
+    List<FoodItem> listFood = new ArrayList<FoodItem>();
+    List<ActionItem> listActive = new ArrayList<ActionItem>();
     TextView todayDate, dayOfTheWeek, countOfDays, targetText;
     Button activityBtn, todayFoodBtn, addFoodBtn;
-    public ListView foodBasketList;
-    public FoodAdapter adapter = new FoodAdapter(getApplicationContext(), initData());
+    ListView foodBasketList, activeBasketList;
+    FoodAdapter adapter; //прихуярю сюда фрагмент чтоб блять можно было запустить обе листвьюхи
+    ActionsAdapter actionsAdapter;
 
-
-    private boolean flag = true;
-    public FragmentManager manager;
-    public FragmentTransaction transaction;
-    public FoodBasketFragment foodBasketFragment;
+    private boolean foodFlag = false, activeFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.today_layout);
 
-        manager = getSupportFragmentManager();
-        foodBasketFragment = new FoodBasketFragment();
-        foodBasketList = (ListView) findViewById(R.id.foodBasketList);
-        foodBasketList.setAdapter(adapter);
+        adapter = new FoodAdapter(this, initFoodData());
+        actionsAdapter = new ActionsAdapter(this, initActiveData());
+
 
         Calendar calendar = Calendar.getInstance();
 
+        foodBasketList = (ListView) findViewById(R.id.my_food_basket);
+        activeBasketList = (ListView) findViewById(R.id.my_active_basket);
+
         activityBtn = (Button) findViewById(R.id.todayActiveBtn);
         todayFoodBtn = (Button) findViewById(R.id.todayFoodBtn);
-        addFoodBtn = (Button) findViewById(R.id.addFoodBtn);
+        //addFoodBtn = (Button) findViewById(R.id.addFoodBtn);
 
         todayDate = (TextView) findViewById(R.id.todayDate);
         dayOfTheWeek = (TextView) findViewById(R.id.todayDayOfTheWeek);
@@ -59,76 +61,134 @@ public class TodayActivity extends FragmentActivity {
         dayOfTheWeek.setText(getDayOfWeek(calendar.get(Calendar.DAY_OF_WEEK)));
     }
 
+
+
     public void onTodayFoodBtnClc(View v){
-        transaction = manager.beginTransaction();
-
-        if(flag == true){
-            transaction.add(R.id.foodBasketFragment, foodBasketFragment); flag = false;}
-        else {transaction.remove(foodBasketFragment);  flag = true;}
-
-        transaction.commit();
+            initFoodData();
+            foodBasketList.setAdapter(adapter);
     }
 
     public  void addFoodClc(View view) {
-        Intent intent = new Intent(getApplicationContext(), FoodCatalogActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(getApplicationContext(), FoodCatalogActivity.class);
+//        startActivity(intent);
     }
 
     public void onTodayActivityBtnClc(View v){
-        Intent intent = new Intent(getApplicationContext(), ActionsCatalogActivity.class);
-        startActivity(intent);
+        initActiveData();
+        activeBasketList.setAdapter(actionsAdapter);
+//        Intent intent = new Intent(getApplicationContext(), ActionsCatalogActivity.class);
+//        startActivity(intent);
     }
 
 
-    public List<FoodItem> initData() {
-            List<FoodItem> list = new ArrayList<FoodItem>();
-            String resp = null;
-            String foodName = null;
-            Float b = new Float(0), j = new Float(0), u = new Float(0), calories = new Float(0);
-            Integer id = 0;
+    private List<FoodItem> initFoodData() {
+        String resp = null;
+        String foodName = null;
+        Float b=new Float(0), j=new Float(0), u=new Float(0), calories=new Float(0);
+        Integer id=0;
 
+
+        try {
+            FileInputStream fin = null;
+            fin = openFileInput("Food.txt");
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            resp = new String(bytes);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        if (resp != null&&foodFlag == true) {
             try {
-                FileInputStream fin = null;
-                fin = openFileInput("Food.txt");
-                byte[] bytes = new byte[fin.available()];
-                fin.read(bytes);
-                resp = new String(bytes);
-            } catch (Exception ex) {
-                Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            if (resp != null) {
-                try {
-                    JSONObject jOb = new JSONObject(resp);
-                    JSONArray jArr = jOb.getJSONArray("food");
-                    for (int i = 0; i < jArr.length(); i++) {
-                        try {
-                            Integer i1 = new Integer(jArr.getJSONObject(i).getString("category_id"));
-                            id = i1;
-                            Float f1 = Float.parseFloat(jArr.getJSONObject(i).getString("protein"));
-                            b = f1;
-                            f1 = Float.parseFloat(jArr.getJSONObject(i).getString("fats"));
-                            j = f1;
-                            f1 = Float.parseFloat(jArr.getJSONObject(i).getString("carbs"));
-                            u = f1;
-                            f1 = Float.parseFloat(jArr.getJSONObject(i).getString("calories"));
-                            calories = f1;
-
-                        } catch (NumberFormatException e) {
-                            System.err.println("Неверный формат строки!");
-                        }
-
-                        foodName = jArr.getJSONObject(i).getString("name");
-
-                        if (foodName != null)
-                            list.add(new FoodItem(foodName, b, j, u, id, calories));
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                JSONObject jOb = new JSONObject(resp);
+                JSONArray jArr = jOb.getJSONArray("food");
+                for (int i = 0; i < 3; i++) {
+//                    try {
+//                        Integer i1 = new Integer(jArr.getJSONObject(i).getString("category_id"));
+//                        id = i1;
+//                        foodName = jArr.getJSONObject(i).getString("name");
+//                        Float f1 = new Float(jArr.getJSONObject(i).getString("protein"));
+//                        b = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("fats"));
+//                        j = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("carbs"));
+//                        u = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("calories"));
+//                        calories = f1;
+//                    } catch (NumberFormatException e) {
+//                        System.err.println("Неверный формат строки!");
+//                    }
+                    //if(b!=0||j!=0||u!=0||calories!=0)
+                    listFood.add(new FoodItem("hui", 1f, 2f, 3f, 4, 1435f));
+                    //list.add(new FoodItem(foodName,b,j,u,id,calories));
                 }
+            } catch (JSONException jEx) {
+                Toast.makeText(getApplicationContext(), jEx.toString(), Toast.LENGTH_SHORT).show();
             }
-            return list;
+            foodFlag = false;
+        } else {
+            while (listFood.isEmpty())
+                listFood.remove(0);
+
+            foodFlag = true;
+        }
+        return listFood;
     }
+
+    private List<ActionItem> initActiveData() {
+        String resp = null;
+        String foodName = null;
+        Float calories=new Float(0);
+        Integer id=0;
+
+
+        try {
+            FileInputStream fin = null;
+            fin = openFileInput("Food.txt");
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            resp = new String(bytes);
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        if (resp != null&&activeFlag == true) {
+            try {
+                JSONObject jOb = new JSONObject(resp);
+                JSONArray jArr = jOb.getJSONArray("food");
+                for (int i = 0; i < 3; i++) {
+//                    try {
+//                        Integer i1 = new Integer(jArr.getJSONObject(i).getString("category_id"));
+//                        id = i1;
+//                        foodName = jArr.getJSONObject(i).getString("name");
+//                        Float f1 = new Float(jArr.getJSONObject(i).getString("protein"));
+//                        b = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("fats"));
+//                        j = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("carbs"));
+//                        u = f1;
+//                        f1 = new Float(jArr.getJSONObject(i).getString("calories"));
+//                        calories = f1;
+//                    } catch (NumberFormatException e) {
+//                        System.err.println("Неверный формат строки!");
+//                    }
+                    //if(b!=0||j!=0||u!=0||calories!=0)
+                    listActive.add(new ActionItem("hui", 3f, 4));
+                    //list.add(new FoodItem(foodName,b,j,u,id,calories));
+                }
+            } catch (JSONException jEx) {
+                Toast.makeText(getApplicationContext(), jEx.toString(), Toast.LENGTH_SHORT).show();
+            }
+            activeFlag = false;
+        } else {
+            while (!listActive.isEmpty())
+                listActive.remove(0);
+
+            activeFlag = true;
+        }
+        return listActive;
+    }
+
 
     private String getDayOfWeek(int i){
         String s="";
