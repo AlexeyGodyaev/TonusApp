ac18749ef8719c57e15d6127fa8718e3403e65f2
BSOD_levch <levch98@mail.ru>
2017-07-07 11:08:18 +0300

    Сервер онлайн, бекап БД

--- backend/Parsing/panda_parse.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-categoryName = 'sport?page=2'
-url = 'http://www.calorizator.ru/product/' + categoryName
-
-for i, df in enumerate(pd.read_html(url)):
-    df.to_csv('sport3_%s.csv' % i, encoding='utf-8')
