a71cc268bf3a3abc2ae0084772a62a572fb575e8
Ryuugamine <Alex.beliayev@gmail.com>
2017-07-12 16:59:29 +0300

    chftm

---
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/ArchiveActivity.java
@@ -0,0 +1,14 @@
+package com.caloriesdiary.caloriesdiary;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ArchiveActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.archive_layout);
+    }
+}
