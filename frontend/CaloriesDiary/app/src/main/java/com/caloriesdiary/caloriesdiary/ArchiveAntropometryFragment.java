b7b112d33394374e4a05bba649014c7f605539c9
Ryuugamine <Alex.beliayev@gmail.com>
2017-07-19 11:49:45 +0300

    графики

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/ArchiveAntropometryFragment.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/ArchiveAntropometryFragment.java
@@ -15,35 +15,35 @@ public class ArchiveAntropometryFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.archive_antropometry_fragment, container, false);
-        rLeg = (TextView) v.findViewById(R.id.right_leg_count);
-        rLeg.setText(getActivity().getIntent().getStringExtra("rLeg"));
+            rLeg = (TextView) v.findViewById(R.id.right_leg_count);
+            rLeg.setText(getActivity().getIntent().getStringExtra("rLeg"));
 
-        shoulders = (TextView) v.findViewById(R.id.shoulders_count);
-        shoulders.setText(getActivity().getIntent().getStringExtra("shoulders"));
+            shoulders = (TextView) v.findViewById(R.id.shoulders_count);
+            shoulders.setText(getActivity().getIntent().getStringExtra("shoulders"));
 
-        lLeg = (TextView) v.findViewById(R.id.left_leg_count);
-        lLeg.setText(getActivity().getIntent().getStringExtra("lLeg"));
+            lLeg = (TextView) v.findViewById(R.id.left_leg_count);
+            lLeg.setText(getActivity().getIntent().getStringExtra("lLeg"));
 
-        rHand = (TextView) v.findViewById(R.id.right_hand_count);
-        rHand.setText(getActivity().getIntent().getStringExtra("rHand"));
+            rHand = (TextView) v.findViewById(R.id.right_hand_count);
+            rHand.setText(getActivity().getIntent().getStringExtra("rHand"));
 
-        lHand = (TextView) v.findViewById(R.id.left_hand_count);
-        lHand.setText(getActivity().getIntent().getStringExtra("lHand"));
+            lHand = (TextView) v.findViewById(R.id.left_hand_count);
+            lHand.setText(getActivity().getIntent().getStringExtra("lHand"));
 
-        butt = (TextView) v.findViewById(R.id.butt_count);
-        butt.setText(getActivity().getIntent().getStringExtra("butt"));
+            butt = (TextView) v.findViewById(R.id.butt_count);
+            butt.setText(getActivity().getIntent().getStringExtra("butt"));
 
-        mass = (TextView) v.findViewById(R.id.mass_count);
-        mass.setText(getActivity().getIntent().getStringExtra("mass"));
+            mass = (TextView) v.findViewById(R.id.mass_count);
+            mass.setText(getActivity().getIntent().getStringExtra("mass"));
 
-        chest = (TextView) v.findViewById(R.id.chest_count);
-        chest.setText(getActivity().getIntent().getStringExtra("chest"));
+            chest = (TextView) v.findViewById(R.id.chest_count);
+            chest.setText(getActivity().getIntent().getStringExtra("chest"));
 
-        waist = (TextView) v.findViewById(R.id.waist_count);
-        waist.setText(getActivity().getIntent().getStringExtra("waist"));
+            waist = (TextView) v.findViewById(R.id.waist_count);
+            waist.setText(getActivity().getIntent().getStringExtra("waist"));
 
-        calves = (TextView) v.findViewById(R.id.calves_count);
-        calves.setText(getActivity().getIntent().getStringExtra("calves"));
+            calves = (TextView) v.findViewById(R.id.calves_count);
+            calves.setText(getActivity().getIntent().getStringExtra("calves"));
 
         return v;
     }
--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/StatActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/StatActivity.java
@@ -2,16 +2,33 @@ package com.caloriesdiary.caloriesdiary;
 
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 
 public class StatActivity extends AppCompatActivity {
-    GraphView graphView;
+    GraphView massGraph, eatedCaloriesGraph, bernCaloriesGraph, rLegGrapg, lLegGraph, calvesGraph, buttGraph, rHandGraph,
+            lHandGraph, chestGraph, shouldersGraph, waistGraph;
+    JSONArray graphArr;
+    DataPoint massData [], eatedData[], bernData [];
+    String graphHor [];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +37,67 @@ public class StatActivity extends AppCompatActivity {
 
         setTitle("Статистика");
 
-        graphView = (GraphView) findViewById(R.id.mass_graph_container);
+        try {
+            JSONObject jsn;
+            File f = new File(getCacheDir(), "Today_params.txt");
+            if (f.exists()) {
+                FileInputStream in = new FileInputStream(f);
+                ObjectInputStream inObject = new ObjectInputStream(in);
+                String text = inObject.readObject().toString();
+                inObject.close();
 
-        LineGraphSeries<DataPoint> exampleSeries = new LineGraphSeries<>(
-                new DataPoint[] { new DataPoint(1, 3.0d),
-                        //new DataPoint(2, 1.5d), new DataPoint(3, 2.5d),
-                        new DataPoint(4, 1.0d), new DataPoint(5, 1.3d) });
 
-        graphView.addSeries(exampleSeries);
+                jsn = new JSONObject(text);
+                graphArr = jsn.getJSONArray("today_params");
+                jsn.remove("today_params");
+            }
+
+
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+        massGraph = (GraphView) findViewById(R.id.mass_graph_container);
+        eatedCaloriesGraph = (GraphView) findViewById(R.id.eated_calories_graph_container);
+        bernCaloriesGraph = (GraphView) findViewById(R.id.bern_calories_graph_container);
+
+        try {
+            massData = new DataPoint[graphArr.length()];
+            eatedData = new DataPoint[graphArr.length()];
+            bernData = new DataPoint[graphArr.length()];
+            graphHor = new String[graphArr.length()];
+
+            for (int i = 0; i < graphArr.length(); i++) {
+                massData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("mass")));
+                eatedData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("eatedCalories")));
+                bernData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("bernCalories")));
+                graphHor[i] = graphArr.getJSONObject(i).getString("date");
+            }
+
+            LineGraphSeries<DataPoint> massSeries = new LineGraphSeries<>(massData);
+            StaticLabelsFormatter massLabelsFormatter = new StaticLabelsFormatter(massGraph);
+            massLabelsFormatter.setHorizontalLabels(graphHor);
+            massGraph.getGridLabelRenderer().setLabelFormatter(massLabelsFormatter);
+
+            massGraph.addSeries(massSeries);
+            massGraph.setTitle("Масса");
+
+            LineGraphSeries<DataPoint> eatedSeries = new LineGraphSeries<>(eatedData);
+            StaticLabelsFormatter eatedLabelsFormatter = new StaticLabelsFormatter(eatedCaloriesGraph);
+            eatedLabelsFormatter.setHorizontalLabels(graphHor);
+            eatedCaloriesGraph.getGridLabelRenderer().setLabelFormatter(eatedLabelsFormatter);
+
+            eatedCaloriesGraph.addSeries(eatedSeries);
+            eatedCaloriesGraph.setTitle("Потребляемые калории");
+
+            LineGraphSeries<DataPoint> bernSeries = new LineGraphSeries<>(bernData);
+            StaticLabelsFormatter bernLabelsFormatter = new StaticLabelsFormatter(bernCaloriesGraph);
+            bernLabelsFormatter.setHorizontalLabels(graphHor);
+            bernCaloriesGraph.getGridLabelRenderer().setLabelFormatter(bernLabelsFormatter);
+
+            bernCaloriesGraph.addSeries(bernSeries);
+            bernCaloriesGraph.setTitle("Затраченные калории");
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
