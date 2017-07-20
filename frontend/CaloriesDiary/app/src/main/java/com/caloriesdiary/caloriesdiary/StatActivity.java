0207f4e46267ebf49cee6540886f1fbf0d5940f2
Ryuugamine <Alex.beliayev@gmail.com>
2017-07-20 12:28:57 +0300

    типа дизигн графиков

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/StatActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/StatActivity.java
@@ -1,6 +1,7 @@
 package com.caloriesdiary.caloriesdiary;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -36,6 +37,8 @@ public class StatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stat_layout);
 
+        int a;
+
         setTitle("Статистика");
 
         try {
@@ -70,19 +73,6 @@ public class StatActivity extends AppCompatActivity {
         chestGraph = (GraphView) findViewById(R.id.chest_graph_container);
         waistGraph = (GraphView) findViewById(R.id.waist_graph_container);
 
-        massGraph.setTitle("Масса");
-        eatedCaloriesGraph.setTitle("Потребляемые калории");
-        bernCaloriesGraph.setTitle("Затраченные калории");
-        shouldersGraph.setTitle("Плечи");
-        waistGraph.setTitle("Талия");
-        chestGraph.setTitle("Грудь");
-        buttGraph.setTitle("Ягодицы");
-        rLegGraph.setTitle("Правое бедро");
-        lLegGraph.setTitle("Левое бедро");
-        rHandGraph.setTitle("Правая рука");
-        lHandGraph.setTitle("Левая рука");
-        calvesGraph.setTitle("Икры");
-
         try {
             if (graphArr!=null&&graphArr.length()>1) {
 
@@ -100,18 +90,80 @@ public class StatActivity extends AppCompatActivity {
                 bernData = new DataPoint[graphArr.length()];
                 graphHor = new String[graphArr.length()];
 
-                for (int i = 0; i < graphArr.length(); i++) {
-                    massData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("mass")));
-                    eatedData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("eatedCalories")));
-                    bernData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("bernCalories")));
-                    rLegData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("rLeg")));
+                if (graphArr.length()>7)
+                    a = graphArr.length()-8; else a = 0;
+
+                for (int i=a; i < graphArr.length(); i++) {
+                    if (graphArr.getJSONObject(i).getString("mass").equals(""))
+                        if(i==0) massData[i] = new DataPoint(i, 0); else
+                             massData[i] = new DataPoint(i, massData[i-1].getY());
+                    else
+                        massData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("mass")));
+
+                    if (graphArr.getJSONObject(i).getString("eatedCalories").equals(""))
+                        if(i==0) eatedData[i] = new DataPoint(i, 0); else
+                            eatedData[i] = new DataPoint(i, eatedData[i-1].getY());
+                    else
+                         eatedData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("eatedCalories")));
+
+                    if (graphArr.getJSONObject(i).getString("bernCalories").equals(""))
+                        if(i==0) bernData[i] = new DataPoint(i, 0); else
+                            bernData[i] = new DataPoint(i, bernData[i-1].getY());
+                    else
+                        bernData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("bernCalories")));
+
+                    if (graphArr.getJSONObject(i).getString("rLeg").equals(""))
+                        if(i==0) rLegData[i] = new DataPoint(i, 0); else
+                            rLegData[i] = new DataPoint(i, rLegData[i-1].getY());
+                    else
+                        rLegData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("rLeg")));
+
+                    if (graphArr.getJSONObject(i).getString("lLeg").equals(""))
+                        if(i==0) lLegData[i] = new DataPoint(i, 0); else
+                            lLegData[i] = new DataPoint(i, lLegData[i-1].getY());
+                    else
                     lLegData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("lLeg")));
+
+                    if (graphArr.getJSONObject(i).getString("rHand").equals(""))
+                        if(i==0) rHandData[i] = new DataPoint(i, 0); else
+                            rHandData[i] = new DataPoint(i, rHandData[i-1].getY());
+                    else
                     rHandData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("rHand")));
+
+                    if (graphArr.getJSONObject(i).getString("lHand").equals(""))
+                        if(i==0) lHandData[i] = new DataPoint(i, 0); else
+                            lHandData[i] = new DataPoint(i, lHandData[i-1].getY());
+                    else
                     lHandData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("lHand")));
+
+                    if (graphArr.getJSONObject(i).getString("waist").equals(""))
+                        if(i==0) waistData[i] = new DataPoint(i, 0); else
+                            waistData[i] = new DataPoint(i, waistData[i-1].getY());
+                    else
                     waistData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("waist")));
+
+                    if (graphArr.getJSONObject(i).getString("chest").equals(""))
+                        if(i==0) chestData[i] = new DataPoint(i, 0); else
+                            chestData[i] = new DataPoint(i, chestData[i-1].getY());
+                    else
                     chestData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("chest")));
+
+                    if (graphArr.getJSONObject(i).getString("shoulders").equals(""))
+                        if(i==0) shouldersData[i] = new DataPoint(i, 0); else
+                            shouldersData[i] = new DataPoint(i, shouldersData[i-1].getY());
+                    else
                     shouldersData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("shoulders")));
+
+                    if (graphArr.getJSONObject(i).getString("butt").equals(""))
+                        if(i==0) buttData[i] = new DataPoint(i, 0); else
+                            buttData[i] = new DataPoint(i,  buttData[i-1].getY());
+                    else
                     buttData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("butt")));
+
+                    if (graphArr.getJSONObject(i).getString("calves").equals(""))
+                        if(i==0) calvesData[i] = new DataPoint(i, 0); else
+                            calvesData[i] = new DataPoint(i, calvesData[i-1].getY());
+                    else
                     calvesData[i] = new DataPoint(i, Double.parseDouble(graphArr.getJSONObject(i).getString("calves")));
                     graphHor[i] = graphArr.getJSONObject(i).getString("date");
                 }
@@ -120,84 +172,132 @@ public class StatActivity extends AppCompatActivity {
                 StaticLabelsFormatter massLabelsFormatter = new StaticLabelsFormatter(massGraph);
                 massLabelsFormatter.setHorizontalLabels(graphHor);
                 massGraph.getGridLabelRenderer().setLabelFormatter(massLabelsFormatter);
-
+                massGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                massGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                massGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                massGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                massSeries.setColor(Color.WHITE);
                 massGraph.addSeries(massSeries);
 
                 LineGraphSeries<DataPoint> eatedSeries = new LineGraphSeries<>(eatedData);
                 StaticLabelsFormatter eatedLabelsFormatter = new StaticLabelsFormatter(eatedCaloriesGraph);
                 eatedLabelsFormatter.setHorizontalLabels(graphHor);
                 eatedCaloriesGraph.getGridLabelRenderer().setLabelFormatter(eatedLabelsFormatter);
-
+                eatedCaloriesGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                eatedCaloriesGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                eatedCaloriesGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                eatedCaloriesGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                eatedSeries.setColor(Color.WHITE);
                 eatedCaloriesGraph.addSeries(eatedSeries);
 
                 LineGraphSeries<DataPoint> bernSeries = new LineGraphSeries<>(bernData);
                 StaticLabelsFormatter bernLabelsFormatter = new StaticLabelsFormatter(bernCaloriesGraph);
                 bernLabelsFormatter.setHorizontalLabels(graphHor);
                 bernCaloriesGraph.getGridLabelRenderer().setLabelFormatter(bernLabelsFormatter);
-
+                bernCaloriesGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                bernCaloriesGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                bernCaloriesGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                bernCaloriesGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                bernSeries.setColor(Color.WHITE);
                 bernCaloriesGraph.addSeries(bernSeries);
 
                 LineGraphSeries<DataPoint> shouldersSeries = new LineGraphSeries<>(shouldersData);
                 StaticLabelsFormatter shouldersLabelsFormatter = new StaticLabelsFormatter(shouldersGraph);
                 shouldersLabelsFormatter.setHorizontalLabels(graphHor);
                 shouldersGraph.getGridLabelRenderer().setLabelFormatter(shouldersLabelsFormatter);
-
+                shouldersGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                shouldersGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                shouldersGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                shouldersGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                shouldersSeries.setColor(Color.WHITE);
                 shouldersGraph.addSeries(shouldersSeries);
 
                 LineGraphSeries<DataPoint> waistSeries = new LineGraphSeries<>(waistData);
                 StaticLabelsFormatter waistLabelsFormatter = new StaticLabelsFormatter(waistGraph);
                 waistLabelsFormatter.setHorizontalLabels(graphHor);
                 waistGraph.getGridLabelRenderer().setLabelFormatter(waistLabelsFormatter);
-
+                waistGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                waistGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                waistGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                waistGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                waistSeries.setColor(Color.WHITE);
                 waistGraph.addSeries(waistSeries);
 
                 LineGraphSeries<DataPoint> chestSeries = new LineGraphSeries<>(chestData);
                 StaticLabelsFormatter chestLabelsFormatter = new StaticLabelsFormatter(chestGraph);
                 chestLabelsFormatter.setHorizontalLabels(graphHor);
                 chestGraph.getGridLabelRenderer().setLabelFormatter(chestLabelsFormatter);
-
+                chestGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                chestGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                chestGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                chestGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                chestSeries.setColor(Color.WHITE);
                 chestGraph.addSeries(chestSeries);
 
                 LineGraphSeries<DataPoint> buttSeries = new LineGraphSeries<>(buttData);
                 StaticLabelsFormatter buttLabelsFormatter = new StaticLabelsFormatter(buttGraph);
                 buttLabelsFormatter.setHorizontalLabels(graphHor);
                 buttGraph.getGridLabelRenderer().setLabelFormatter(buttLabelsFormatter);
-
+                buttGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                buttGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                buttGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                buttGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                buttSeries.setColor(Color.WHITE);
                 buttGraph.addSeries(buttSeries);
 
                 LineGraphSeries<DataPoint> rLegSeries = new LineGraphSeries<>(rLegData);
                 StaticLabelsFormatter rLegLabelsFormatter = new StaticLabelsFormatter(rLegGraph);
                 rLegLabelsFormatter.setHorizontalLabels(graphHor);
                 rLegGraph.getGridLabelRenderer().setLabelFormatter(rLegLabelsFormatter);
-
+                rLegGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                rLegGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                rLegGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                rLegGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                rLegSeries.setColor(Color.WHITE);
                 rLegGraph.addSeries(rLegSeries);
 
                 LineGraphSeries<DataPoint> lLegSeries = new LineGraphSeries<>(lLegData);
                 StaticLabelsFormatter lLegLabelsFormatter = new StaticLabelsFormatter(lLegGraph);
                 lLegLabelsFormatter.setHorizontalLabels(graphHor);
                 lLegGraph.getGridLabelRenderer().setLabelFormatter(lLegLabelsFormatter);
-
+                lLegGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                lLegGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                lLegGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                lLegGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                lLegSeries.setColor(Color.WHITE);
                 lLegGraph.addSeries(lLegSeries);
 
                 LineGraphSeries<DataPoint> rHandSeries = new LineGraphSeries<>(rHandData);
                 StaticLabelsFormatter rHandLabelsFormatter = new StaticLabelsFormatter(rHandGraph);
                 rHandLabelsFormatter.setHorizontalLabels(graphHor);
                 rHandGraph.getGridLabelRenderer().setLabelFormatter(rHandLabelsFormatter);
-
+                rHandGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                rHandGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                rHandGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                rHandGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                rHandSeries.setColor(Color.WHITE);
                 rHandGraph.addSeries(rHandSeries);
 
                 LineGraphSeries<DataPoint> lHandSeries = new LineGraphSeries<>(lHandData);
                 StaticLabelsFormatter lHandLabelsFormatter = new StaticLabelsFormatter(lHandGraph);
                 lHandLabelsFormatter.setHorizontalLabels(graphHor);
                 lHandGraph.getGridLabelRenderer().setLabelFormatter(lHandLabelsFormatter);
-
+                lHandGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                lHandGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                lHandGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                lHandGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                lHandSeries.setColor(Color.WHITE);
                 lHandGraph.addSeries(lHandSeries);
-
+                
                 LineGraphSeries<DataPoint> calvesSeries = new LineGraphSeries<>(calvesData);
                 StaticLabelsFormatter calvesLabelsFormatter = new StaticLabelsFormatter(calvesGraph);
                 calvesLabelsFormatter.setHorizontalLabels(graphHor);
                 calvesGraph.getGridLabelRenderer().setLabelFormatter(calvesLabelsFormatter);
-
+                calvesGraph.setBackgroundColor(Color.parseColor("#A5CDCD"));
+                calvesGraph.getGridLabelRenderer().setGridColor(Color.WHITE);
+                calvesGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.WHITE);
+                calvesGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
+                calvesSeries.setColor(Color.WHITE);
                 calvesGraph.addSeries(calvesSeries);
             }
             else Toast.makeText(this, "Статистика доступна после двух заполненных дней", Toast.LENGTH_LONG).show();
