665078b0bf1f1ee32b11ff48e62dcc0d2d37125e
HNKOP <emilgasanov@mail.ru>
2017-07-27 10:34:14 +0300

    Табы

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/FoodBuilderActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/FoodBuilderActivity.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 
@@ -20,9 +21,33 @@ public class FoodBuilderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_builder_layout);
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        // инициализация
+        tabHost.setup();
+        TabHost.TabSpec tabSpec;
 
+        // создаем вкладку и указываем тег
+        tabSpec = tabHost.newTabSpec("tag1");
+        // название вкладки
+        tabSpec.setIndicator("Вкладка 1");
+        // указываем id компонента из FrameLayout, он и станет содержимым
+        tabSpec.setContent(R.id.tab1);
+        // добавляем в корневой элемент
+        tabHost.addTab(tabSpec);
 
+        tabSpec = tabHost.newTabSpec("tag2");
+        // указываем название и картинку
+        // в нашем случае вместо картинки идет xml-файл,
+        tabSpec.setIndicator("Вкладка 2");
+        // который определяет картинку по состоянию вкладки
+        tabSpec.setContent(R.id.tab2);
+        tabHost.addTab(tabSpec);
 
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            public void onTabChanged(String tabId) {
+                Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     public void onAddFood (View view)
     {
