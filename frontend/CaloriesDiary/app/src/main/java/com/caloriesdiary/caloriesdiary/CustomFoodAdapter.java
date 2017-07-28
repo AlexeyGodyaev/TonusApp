cf9211ccaf176d8495b04aee5d4529c295bce570
HNKOP <emilgasanov@mail.ru>
2017-07-28 09:19:10 +0300

    tjy

---
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/CustomFoodAdapter.java
@@ -0,0 +1,55 @@
+package com.caloriesdiary.caloriesdiary;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class CustomFoodAdapter extends RecyclerView.Adapter<CustomFoodAdapter.ViewHolder>  {
+    private List<FoodItem> listitem;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
+
+        public CheckBox checkBox;
+        public ViewHolder(View view) {
+            super(view);
+            checkBox = (CheckBox) view.findViewById(R.id.add_food_checkbox);
+            view.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+
+        }
+
+    }
+
+
+    public CustomFoodAdapter(List<FoodItem> items) {
+        listitem = items;
+    }
+
+    @Override
+    public CustomFoodAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.add_food_dialog_recycle_item,parent,false);
+
+        CustomFoodAdapter.ViewHolder vh = new CustomFoodAdapter.ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(CustomFoodAdapter.ViewHolder holder, int position) {
+        FoodItem foodItem = listitem.get(position);
+        holder.checkBox.setText(foodItem.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listitem.size();
+    }
+}
--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/FoodBuilderActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/FoodBuilderActivity.java
@@ -2,20 +2,51 @@ package com.caloriesdiary.caloriesdiary;
 
 
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 
 public class FoodBuilderActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
+    private RecyclerView dialogRecyclerView;
+    private RecyclerView.Adapter dialogAdapter;
+    private RecyclerView.LayoutManager dialogLayoutManager;
+
+    List<FoodItem> list = new ArrayList<>();
+    GetFood get;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +60,7 @@ public class FoodBuilderActivity extends AppCompatActivity {
         // создаем вкладку и указываем тег
         tabSpec = tabHost.newTabSpec("tag1");
         // название вкладки
-        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setIndicator("Справочник");
         // указываем id компонента из FrameLayout, он и станет содержимым
         tabSpec.setContent(R.id.tab1);
         // добавляем в корневой элемент
@@ -38,7 +69,7 @@ public class FoodBuilderActivity extends AppCompatActivity {
         tabSpec = tabHost.newTabSpec("tag2");
         // указываем название и картинку
         // в нашем случае вместо картинки идет xml-файл,
-        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setIndicator("Холодильник");
         // который определяет картинку по состоянию вкладки
         tabSpec.setContent(R.id.tab2);
         tabHost.addTab(tabSpec);
@@ -48,23 +79,202 @@ public class FoodBuilderActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mRecyclerView =  (RecyclerView) findViewById(R.id.food_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+//        mAdapter = new RecycleFoodAdapter(initData());
+//        mRecyclerView.setAdapter(mAdapter);
+
+        get = new FoodBuilderActivity.GetFood();
+        get.execute("http://caloriesdiary.ru/food/get_food",String.valueOf(-1));
+
+
     }
     public void onAddFood (View view)
     {
         View content = LayoutInflater.from(getApplicationContext()).inflate(R.layout.add_food_dialoglayout,null);
         final EditText edittext = (EditText) content.findViewById(R.id.custom_food_name);
+
+        dialogRecyclerView = (RecyclerView) content.findViewById(R.id.custom_food_recycle);
+        dialogRecyclerView.setHasFixedSize(true);
+
+        dialogLayoutManager = new LinearLayoutManager(this);
+        dialogRecyclerView.setLayoutManager(dialogLayoutManager);
+
+        dialogAdapter = new CustomFoodAdapter(list);
+        dialogRecyclerView.setAdapter(dialogAdapter);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(FoodBuilderActivity.this);
         builder.setTitle("Добавление блюда")
                 .setView(content)
-                .setPositiveButton("Сохранить", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Принять", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Toast.makeText(FoodBuilderActivity.this, edittext.getText().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
                     }
                 });
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    private class GetFood extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(FoodBuilderActivity.this, "Загрузка блюд...", Toast.LENGTH_SHORT).show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+
+            JSONArray resp = null;
+            String foodName = null;
+            Float b=0f, j=0f, u=0f, calories=0f;
+            Integer id=0;
+
+            try {
+
+                String answer = get.get();
+                try
+                {
+                    resp = new JSONArray(answer);
+                }
+                catch (Exception e)
+                {
+                   //
+                }
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                Toast.makeText(FoodBuilderActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+            if (resp != null)
+                for(int i = 0; i<resp.length(); i++){
+                    try {
+                        id = Integer.valueOf(resp.getJSONObject(i).getString("category_id"));
+                        foodName = resp.getJSONObject(i).getString("name");
+                        b = Float.valueOf(resp.getJSONObject(i).getString("protein"));
+                        j = Float.valueOf(resp.getJSONObject(i).getString("fats"));
+                        u = Float.valueOf(resp.getJSONObject(i).getString("carbs"));
+                        calories = Float.valueOf(resp.getJSONObject(i).getString("calories"));
+                    } catch (NumberFormatException e) {
+                        System.err.println("Неверный формат строки!");
+                    } catch (JSONException jEx){
+                        Toast.makeText(getApplicationContext(),jEx.toString(), Toast.LENGTH_SHORT).show();
+                        //errors.setText(jEx.toString());
+                    }
+                    if(b!=0||j!=0||u!=0||calories!=0)
+                        list.add(new FoodItem(foodName,b,j,u,id,calories));
+                }
+
+            mAdapter = new RecycleFoodAdapter(list);
+            mRecyclerView.setAdapter(mAdapter);
+            Toast.makeText(FoodBuilderActivity.this, "Загружено элементов: " + String.valueOf(list.size()), Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                URL url = new URL(strings[0]); // первый аргумент из массива который передан при вызове
+                JSONObject postDataParams = new JSONObject();
+
+                postDataParams.put("offset",strings[1]);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(150000 /* milliseconds */);
+                conn.setConnectTimeout(150000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams)); // преобразуем json объект в строку параметров запроса
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    BufferedReader in=new BufferedReader(
+                            new InputStreamReader(
+                                    conn.getInputStream(), "UTF-8"));
+                    //StringBuffer sb = new StringBuffer("");
+                    String line;
+                    try
+                    {
+                        JSONObject js = null;
+                        while((line = in.readLine()) != null) {
+                            js = new JSONObject(line);
+                            break;
+                        }
+                        JSONArray jArr = js.getJSONArray("food");
+
+                        in.close();
+                        return jArr.toString();
+                    }
+                    catch (Exception e)
+                    {
+                        return "JSONparse error: " + e.toString();
+                    }
+
+
+
+                }
+                else {
+                    return "Response error: " + String.valueOf(responseCode);
+                }
+            }
+            catch(Exception e){
+                return "Connection error: " + e.toString();
+            }
+
+        }
+        private String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while(itr.hasNext()){
+
+                String key= itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
     }
 
 
