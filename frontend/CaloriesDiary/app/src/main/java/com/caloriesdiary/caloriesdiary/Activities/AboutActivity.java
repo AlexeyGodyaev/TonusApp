d76efe2fb00b7169034d2041d18e7914b22cce56
Ryuugamine <Alex.beliayev@gmail.com>
2017-09-08 17:04:14 +0300

    дл

--- frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/Activities/AboutActivity.java
+++ frontend/CaloriesDiary/app/src/main/java/com/caloriesdiary/caloriesdiary/Activities/AboutActivity.java
@@ -20,6 +20,7 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
